033f371b437bb469ebdd81c0ef9164cf0de1cbb9
inokawa <48897392+inokawa@users.noreply.github.com>
2021-03-14 16:43:15 +0900

    Impl createInstance

--- src/custom-renderer/reconciler.js
+++ src/custom-renderer/reconciler.js
@@ -3,7 +3,18 @@ import ReactReconciler from "react-reconciler";
 export const reconciler = ReactReconciler({
   supportsMutation: true,
 
-  createInstance() {},
+  createInstance(
+    type,
+    props,
+    rootContainerInstance,
+    hostContext,
+    internalInstanceHandle
+  ) {
+    const el = document.createElement(type);
+    if (props.className) el.className = props.className;
+    if (props.src) el.src = props.src;
+    return el;
+  },
   createTextInstance() {},
 
   appendChildToContainer() {},
