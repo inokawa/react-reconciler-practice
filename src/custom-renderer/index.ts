27feb51aa505d22aaee1e0710a6464b35b5a6621
inokawa <48897392+inokawa@users.noreply.github.com>
2021-03-14 20:38:26 +0900

    Update index.ts

--- src/custom-renderer/index.ts
+++ src/custom-renderer/index.ts
@@ -4,6 +4,6 @@ import { reconciler } from "./reconciler";
 export default {
   render(whatToRender: React.ReactNode, div: HTMLElement | null): void {
     const container = reconciler.createContainer(div, 0, false, null);
-    reconciler.updateContainer(whatToRender, container, null, () => {});
+    reconciler.updateContainer(whatToRender, container, null, () => undefined);
   },
 };
