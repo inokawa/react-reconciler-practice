59f64c37ca364d7c73539c07baae6698d0cac685
inokawa <48897392+inokawa@users.noreply.github.com>
2021-03-14 18:15:14 +0900

    Fix tsc errors

--- src/custom-renderer/index.ts
+++ src/custom-renderer/index.ts
@@ -3,9 +3,7 @@ import { reconciler } from "./reconciler";
 
 export default {
   render(whatToRender: React.ReactNode, div: HTMLElement | null): void {
-    // @ts-expect-error
-    const container = reconciler.createContainer(div, false, false, null);
-    // @ts-expect-error
-    reconciler.updateContainer(whatToRender, container, null, null);
+    const container = reconciler.createContainer(div, 0, false, null);
+    reconciler.updateContainer(whatToRender, container, null, () => {});
   },
 };
