926735465e60b120227a07ff7fe084cb0e3b1931
inokawa <48897392+inokawa@users.noreply.github.com>
2021-03-16 01:07:14 +0900

    Handle multiple containers

--- src/custom-renderer/index.ts
+++ src/custom-renderer/index.ts
@@ -1,9 +1,17 @@
 import React from "react";
 import { reconciler } from "./reconciler";
 
+const containers = new Map<HTMLElement, any>();
+
 export default {
-  render(elem: React.ReactNode, dom: HTMLElement | null): void {
-    const container = reconciler.createContainer(dom, 0, false, null);
+  render(elem: React.ReactNode, root: HTMLElement | null): void {
+    if (!root) return;
+    let container = containers.get(root);
+    if (!container) {
+      container = reconciler.createContainer(root, 0, false, null);
+      containers.set(root, container);
+    }
+
     reconciler.updateContainer(elem, container, null, () => undefined);
   },
 };
